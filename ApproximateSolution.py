33fada20363ad0a0e6d8314cc1338aefd365a1d1
DADOU STEVEN <dgv1pro@gmail.com>
2023-08-04 09:24:15 +0200

    dadou

--- ApproximateSolution.py
+++ ApproximateSolution.py
@@ -1,7 +1,9 @@
+# cube root
+
 cube = 27
 epsilon = 0.01
 guess = 0.0
-increment = 0.0001
+increment = 0.01
 num_guesses = 0
 
 while abs(guess ** 3 - cube) >= epsilon:
@@ -12,3 +14,23 @@ if abs(guess ** 3 - cube) >= epsilon:
     print('Failed on cube root of', cube)
 else:
     print(guess, 'is close to the cube root of', cube)
+
+
+# square root
+
+x = 25
+low = 1.0
+high = x
+ans = (high + low) / 2.0
+print("")
+
+while abs(ans ** 2 - x) >= epsilon:
+    print('low = ', low, "high = ", high, "ans = ", ans)
+    num_guesses += 1
+    if ans ** 2 < x:
+        low = ans
+    else:
+        high = ans
+    ans = (high + low) / 2.0
+print("numGuesses = ", num_guesses)
+print(ans, "is close to square root of", x)
